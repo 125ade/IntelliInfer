14e2e78544f903c742a3e53339c2d21a05153d96
Andrea <24610906+125ade@users.noreply.github.com>
2024-05-28 18:45:42 +0200

    start create infrastructure of the project and fix structure of the repo

---
+++ backend/api/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { Request, Response } from 'express';
+
+// Crea un'applicazione Express
+const app = express();
+const port = 3000;
+
+// Definisce la route per il controllo dello stato
+app.get('/check/health', (req: Request, res: Response) => {
+  res.json({ system: 'online' });
+});
+
+
+
+// Avvia il server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
