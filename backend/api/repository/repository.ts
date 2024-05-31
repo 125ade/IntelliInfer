b4f8616d2b59e42489145a2d4c0ba4ac49070b1d
MicolZazzarini <micol.zazzarini3@gmail.com>
2024-05-31 13:50:22 +0200

    updated repository methods and ErrorCode.ts

--- backend/api/repository/repository.ts
+++ backend/api/repository/repository.ts
@@ -1,34 +1,35 @@
 import UserDao from '../dao/userDao';
-import ImageDao from '../dao/imageDao'
-import DatasetDao from '../dao/datasetDao'
 import TagDao from '../dao/tagDao';
-import LabelDao from '../dao/labelDao'
-import ResultDao from '../dao/resultDao'
 import Tag from '../models/tag';
+import DatasetDao from '../dao/datasetDao';
+import ImageDao from '../dao/imageDao';
+import Image from '../models/image';
 import { isImage, unzipImages } from '../utils/utils'; // Importa le funzioni di utilità
 import { SequelizeConnection } from '../db/SequelizeConnection';
 import { ConcreteErrorCreator } from '../factory/ErrorCreator';
 import * as fs from 'fs';
+import Dataset from '../models/dataset';
+
 
 
 export interface IRepository {
     createTags(tags: string[], datasetId: number): Promise<Tag[]>;
     createDataset(datasetJson: any): Promise<Object>;
-    //uploadFile(datasetId: number, filePath: string): Promise<Image[]>;
-    //updateUserTokenByCost(user: User, cost: number): Promise<void>;
-    //checkUserToken(user: User, amount: number): void;
-    //updateUserToken(user: User, token: number): Promise<Object> ;
+    uploadFile(datasetId: number, filePath: string): Promise<Image[]>;
+    updateUserTokenByCost(userId: number, cost: number): Promise<void>;
+    checkUserToken(userId: number, amount: number): void;
+    updateUserToken(userId: number, token: number): Promise<Object> ;
 }
 
 
 class Repository implements IRepository {
 
     // all actions require a default user
-    private user: UserDao;
+    // private user: UserDao;
 
-    constructor(user: UserDao) {
-       this.user = user;
-    }
+    //constructor(user: UserDao) {
+    //   this.user = user;
+    //}
 
     // method to create tags associated with a specific dataset
     public async createTags(tags: string[], datasetId: number): Promise<Tag[]> {
@@ -39,9 +40,10 @@ class Repository implements IRepository {
         return createdTags;
     }
 
-    /** 
-    public async createDataset(datasetJson: any): Promise<{ dataset: DatasetDao, tags: Tag[] }> {
+    // funzione da ricontrollare
+    public async createDataset(datasetJson: any): Promise<{ dataset: Dataset, tags: Tag[] }> {
         try {
+          // quando passo il json con il dataset passo anche il tag o i tags che voglio associargli
           let tags = datasetJson.tags;
     
           // set creation and update date
@@ -50,94 +52,101 @@ class Repository implements IRepository {
           datasetJson.updatedAt = now;
     
           // add user id
-          datasetJson.userId = this.user.id;
+          //datasetJson.userId = this.user.id;
     
           // Crea il dataset nel database
-          const dataset = await Dataset.create(datasetJson);
+          const datasetDao = new DatasetDao();
+          const dataset = await datasetDao.create(datasetJson);
     
           // Crea i tag, rimuovendo i duplicati
           const uniqueTags: string[] = [...new Set(tags as string[])];
           const createdTags = await this.createTags(uniqueTags, dataset.id);
     
           return { dataset: dataset, tags: createdTags };
-        } catch (error) {
-          console.error('Error creating dataset:', error);
-          throw new Error('Failed to create dataset');
+        } catch {
+            throw new ConcreteErrorCreator().createServerError().setFailedCreationItem();
         }
     }
     
-
+    
     // Ho provato a creare un metodo che verrà usato nella rotta per l'upload di un file nel dataset
     // a seconda che il file sia un'immagine o un file zip richiama le funzioni di utilità per verificare
     // che il file sia un immagine o nel caso in cui sia un file zip eseguire l'unzip
     public async uploadFile(datasetId: number, filePath: string) {
 
         const sequelize = SequelizeConnection.getInstance().sequelize;
-
-        const t = await sequelize.transaction();
         try {
             let images;
+            const imageDao = new ImageDao();
 
             if(isImage(filePath)) {
-                const image = await Image.create({
+                const image = await imageDao.create({
                     datasetId: datasetId,
                     path: filePath
-                }, { transaction: t });
+                });
                 images = [image];
             } else {
                 const bufferList = await unzipImages(filePath);
                 images = await Promise.all(bufferList.map(async (buffer: any) => {
                     const tempFilePath = `${filePath}-${Date.now()}.img`; // Nome temporaneo per il file immagine
                     fs.writeFileSync(tempFilePath, buffer); // Scrivi il buffer su un file temporaneo
-                    const img = await Image.create({
+                    const img = await imageDao.create({
                         dataset_id: datasetId,
                         path: tempFilePath // Salva il percorso del file temporaneo
-                    }, { transaction: t });
+                    });
                     return img;
                 }));
             }
 
             // Aggiorna il conteggio degli elementi nel dataset
-            await Dataset.increment('count_elements', { where: { id: datasetId }, transaction: t });
-
-            // Commit della transazione
-            await t.commit();
-
+            await Dataset.increment('count_elements', { where: { id: datasetId }});
             return images;
         } catch (error) {
-            // Rollback della transazione in caso di errore
-            await t.rollback();
-            throw error;
+            throw new ConcreteErrorCreator().createServerError().setFailedUploadFile();
         }
-    }
-
+    } 
 
     // updates the user token amount subtracting a cost
     // checks if the user has the available amount
-    public async updateUserTokenByCost(user: User, cost: number): Promise<void> {
-        this.checkUserToken(user, cost);
-
-        try {
-           await this.user.set({ token: this.user.token - cost }).save();
-        } catch (err) {
-           throw new ConcreteErrorCreator().createServerError().setUpdatingToken();
+    public async updateUserTokenByCost(userId: number, cost: number): Promise<void> {
+        const userDao = new UserDao();
+        const user = await userDao.findById(userId);
+        this.checkUserToken(userId, cost);
+        if(user !== null){
+            try {
+                await user.set({ token: user.token - cost }).save();
+             } catch {
+                throw new ConcreteErrorCreator().createServerError().setUpdatingToken();
+             }
+        } else{ 
+            throw new ConcreteErrorCreator().createNotFoundError().setNoUser();
         }
     }
 
     // checks if the user token amount is >= requested amount
-    checkUserToken(user: User, amount: number): void {
-        if (user.token < amount)
-            throw new ConcreteErrorCreator().createForbiddenError().setNeedMoreToken(amount);
+    async checkUserToken(userId: number, amount: number): Promise<void> {
+        const userDao = new UserDao();
+        const user = await userDao.findById(userId);
+        if (user !== null && user.token < amount)
+            throw new ConcreteErrorCreator().createForbiddenError().setInsufficientToken(amount);
     }
 
     // updates the token amount of a specified user
     // returns the updated user
-    async updateUserToken(user: User, token: number): Promise<Object> {
-        await user.set({ token: token }).save();
-
+    async updateUserToken(userId: number, token: number): Promise<Object> {
+        const userDao = new UserDao();
+        const user = await userDao.findById(userId);
+        if(user){
+            await user.set({ token: token }).save();
+        }else{
+            throw new ConcreteErrorCreator().createNotFoundError().setNoUser();
+        }
         return { updatedUser: user };
     }
-*/
+
+    // Voglio creare un metodo che aggiorni il tag di un dataset, devo utilizzare un DAO di DatasetTag???
+    
+
 }
 // todo: methods to update model weights
 
