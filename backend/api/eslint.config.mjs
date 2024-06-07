c21a5939cd1730657a39db11273988b45df89c0d
Andrea <24610906+125ade@users.noreply.github.com>
2024-06-07 22:29:07 +0200

    delete problem

--- backend/api/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-
-
-export default [
-  {files: ["**/*.ts"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
-  ...tseslint.configs.recommended,
-];
