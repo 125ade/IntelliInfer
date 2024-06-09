17ee27dcfd34e74744ffd46281d732cf5db0d29d
Andrea <24610906+125ade@users.noreply.github.com>
2024-06-09 16:06:21 +0200

    checkpoint 3 save work in progress

--- backend/api/db/dbSync.ts
+++ backend/api/db/dbSync.ts
@@ -1,7 +1,6 @@
 import Ai from "../models/ai";
 import Dataset from "../models/dataset";
 import Image from "../models/image";
-import Label from "../models/label";
 import Result from "../models/result";
 import Tag from "../models/tag";
 import User from "../models/user";
@@ -12,10 +11,9 @@ const syncDb = async () => {
       await Image.sync();
       await Ai.sync();
       await Dataset.sync();
-      await Label.sync();
       await Result.sync();
       await Tag.sync();
 };
   
-export { User, Image, Ai, Dataset, Label, Result, Tag, syncDb };
+export { User, Image, Ai, Dataset, Result, Tag, syncDb };
 
