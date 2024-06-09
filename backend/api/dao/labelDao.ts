d75d75c08d86566f9b53cacee9f24d6e38a86083
MicolZazzarini <micol.zazzarini3@gmail.com>
2024-06-09 10:14:15 +0200

    deleted labels

--- backend/api/dao/labelDao.ts
+++
@@ -1,9 +0,0 @@
-import { IDao } from './daoInterface';
-import Label from '../models/label';
-
-// TO DO: ELIMINARE 
-export default class LabelDAO implements IDao<Label> {
-
-    constructor() {}
-    
-}
